5d2e383ec2abffa1a7baa6c34bde85135e074a1f
Rohan Somvanshi <rsomvanshi@nvidia.com>
2023-05-08 12:04:12 -0400

    add monitoring link

--- mlflow/server/js/src/experiment-tracking/components/modals/GetLinkModal.js
+++ mlflow/server/js/src/experiment-tracking/components/modals/GetLinkModal.js
@@ -6,15 +6,20 @@ import { CopyBox } from '../../../shared/building_blocks/CopyBox';
 
 export const GetLinkModal = ({ visible, onCancel, link }) => {
   return (
-    <Modal
-      title={
-        <FormattedMessage defaultMessage='Get Link' description={'Title text for get-link modal'} />
-      }
-      visible={visible}
-      onCancel={onCancel}
-    >
-      <CopyBox copyText={link} />
-    </Modal>
+      <div>
+        <Modal
+          title={
+            <FormattedMessage defaultMessage='Get Link' description={'Title text for get-link modal'} />
+          }
+          visible={visible}
+          onCancel={onCancel}
+        >
+          <CopyBox copyText={link} />
+        </Modal>
+        <div>
+            For monitoring: <a href="https://ops-xpva.kratos.nvidia.com/d/kratos_xp_k8_namespace_pods/kratos-xp-kubernetes-namespace-pods?orgId=1&refresh=30s&var-datasource=Prometheus&var-namespace=kratos-team" target="_blank" rel="noopener noreferrer">click here</a>
+        </div>
+      </div>
   );
 };
 
